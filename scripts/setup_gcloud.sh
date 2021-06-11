ce21401ebd1d013c3695dd35fbe1236bfa50afff
Stepan Legachev <gitlog@sib.li>
2021-06-11 16:56:11 +0300

    refactor: gcloud setup script
    (cherry picked from commit 993fb4c20fb26cffc1a6282f19d3ca5e09d05d84)

--- scripts/setup_gcloud.sh
+++ scripts/setup_gcloud.sh
@@ -4,7 +4,10 @@
 
 set -euo pipefail
 
-BASEDIR="$( cd "$(dirname "$0")" || true ; pwd -P )"
+BASEDIR="$(
+  cd "$(dirname "$0")" || true
+  pwd -P
+)"
 
 GOOGLE_CLOUD_PROJECT=${1:-$(gcloud config list --format 'value(core.project)')}
 
@@ -13,33 +16,37 @@ SERVICE_ACCOUNT=github-actions-deployment
 
 SERVICE_ACCOUNT_EMAIL=${SERVICE_ACCOUNT}@${GOOGLE_CLOUD_PROJECT}.iam.gserviceaccount.com
 
-if gcloud iam roles describe "${ROLE_ID}" --project="${GOOGLE_CLOUD_PROJECT}" > /dev/null 2>&1; then
-    echo "✓ Found Role ${ROLE_ID} in project ${GOOGLE_CLOUD_PROJECT}"
+bld='\033[1m'
+clr='\033[0m'
+
+if gcloud iam roles describe "${ROLE_ID}" --project="${GOOGLE_CLOUD_PROJECT}" >/dev/null 2>&1; then
+  echo -e "✓ Found Role $bld${ROLE_ID}$clr in Project $bld${GOOGLE_CLOUD_PROJECT}$clr"
 else
-    echo "… Creating Role ${ROLE_ID} in project ${GOOGLE_CLOUD_PROJECT}"
-    gcloud iam roles create "${ROLE_ID}" --file="$BASEDIR/appengine_deployer_role.yml" --project="${GOOGLE_CLOUD_PROJECT}"
+  echo -e "… Creating Role $bld${ROLE_ID}$clr in Project $bld${GOOGLE_CLOUD_PROJECT}$clr"
+  gcloud iam roles create "${ROLE_ID}" --file="$BASEDIR/appengine_deployer_role.yml" --project="${GOOGLE_CLOUD_PROJECT}"
 fi
 
-if gcloud iam service-accounts describe "${SERVICE_ACCOUNT_EMAIL}" --project="${GOOGLE_CLOUD_PROJECT}" > /dev/null 2>&1; then
-  echo "✓ Found Service Account ${SERVICE_ACCOUNT_EMAIL}"
+if gcloud iam service-accounts describe "${SERVICE_ACCOUNT_EMAIL}" --project="${GOOGLE_CLOUD_PROJECT}" >/dev/null 2>&1; then
+  echo -e "✓ Found Service Account $bld${SERVICE_ACCOUNT_EMAIL}$clr"
 else
-  echo "… Creating Service Account ${SERVICE_ACCOUNT_EMAIL}"
+  echo -e "… Creating Service Account $bld${SERVICE_ACCOUNT_EMAIL}$clr"
   gcloud iam service-accounts create "${SERVICE_ACCOUNT}" --display-name="${SERVICE_ACCOUNT}" --description="Account to deploy via GitHub Actions" --project="${GOOGLE_CLOUD_PROJECT}"
 fi
 
-EXISTING_ROLE_BINDING=$(gcloud projects  get-iam-policy "${GOOGLE_CLOUD_PROJECT}" --flatten="bindings[].members" --format="value(bindings.role)" --filter="bindings.role:roles/${ROLE_ID} AND bindings.members:serviceAccount:${SERVICE_ACCOUNT_EMAIL}")
+EXISTING_ROLE_BINDING=$(gcloud projects get-iam-policy "${GOOGLE_CLOUD_PROJECT}" --flatten="bindings[].members" --format="value(bindings.role)" --filter="bindings.role:roles/${ROLE_ID} AND bindings.members:serviceAccount:${SERVICE_ACCOUNT_EMAIL}")
 if [ "${EXISTING_ROLE_BINDING}" = "projects/${GOOGLE_CLOUD_PROJECT}/roles/${ROLE_ID}" ]; then
-    echo "✓ Binding exists: Role ${ROLE_ID} is already attached to Service Account ${SERVICE_ACCOUNT}"
+  echo -e "✓ Found IAM policy binding for $bld${SERVICE_ACCOUNT}$clr with Role $bld${ROLE_ID}$clr"
 else
-    echo "… Creating IAM policy binding for ${SERVICE_ACCOUNT} and Role ${ROLE_ID}"
-    gcloud projects add-iam-policy-binding "${GOOGLE_CLOUD_PROJECT}" --member="serviceAccount:${SERVICE_ACCOUNT_EMAIL}" --role="projects/${GOOGLE_CLOUD_PROJECT}/roles/${ROLE_ID}" --condition=None
+  echo -e "… Creating IAM policy binding for $bld${SERVICE_ACCOUNT}$clr and Role $bld${ROLE_ID}$clr"
+  gcloud projects add-iam-policy-binding "${GOOGLE_CLOUD_PROJECT}" --member="serviceAccount:${SERVICE_ACCOUNT_EMAIL}" --role="projects/${GOOGLE_CLOUD_PROJECT}/roles/${ROLE_ID}" --condition=None
 fi
 
-echo "… Creating new private key for Service Account ${SERVICE_ACCOUNT_EMAIL}"
-TMP_OUTPUT_FILE="$(mktemp)"
-gcloud iam service-accounts keys create "${TMP_OUTPUT_FILE}" --iam-account "github-actions-deployment@${GOOGLE_CLOUD_PROJECT}.iam.gserviceaccount.com"
+echo -e "… Creating new private key for $bld${SERVICE_ACCOUNT}$clr"
 
-printf "\nGCLOUD_PROJECT_ID: %s\n" "${GOOGLE_CLOUD_PROJECT}"
-printf "\nGCP_SA_EMAIL: %s\n" "github-actions-deployment@${GOOGLE_CLOUD_PROJECT}.iam.gserviceaccount.com"
-printf "\nGOOGLE_APPLICATION_CREDENTIALS (copy all lines):\n\n%s\n\n" "$(base64 "${TMP_OUTPUT_FILE}")"
+TMP_OUTPUT_FILE="$(mktemp)"
+gcloud iam service-accounts keys create "${TMP_OUTPUT_FILE}" --iam-account "${SERVICE_ACCOUNT_EMAIL}"
+
+printf "\n${bld}GCLOUD_PROJECT_ID${clr}: %s\n" "${GOOGLE_CLOUD_PROJECT}"
+printf "\n${bld}GCP_SA_EMAIL${clr}: %s\n" "${SERVICE_ACCOUNT_EMAIL}"
+printf "\n${bld}GOOGLE_APPLICATION_CREDENTIALS${clr} (copy all lines):\n\n%s\n\n" "$(base64 "${TMP_OUTPUT_FILE}")"
 rm "${TMP_OUTPUT_FILE}"
