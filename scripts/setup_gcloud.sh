201476d7218c25ad3b7dc46649ce94d2d3ed1831
Stepan Legachev <gitlog@sib.li>
2021-06-11 16:53:53 +0300

    refactor: gcloud setup script
    (cherry picked from commit c8fda376abb8568e75926ff5eabadbed4faa51cd)

--- scripts/setup_gcloud.sh
+++ scripts/setup_gcloud.sh
@@ -8,6 +8,11 @@ BASEDIR="$( cd "$(dirname "$0")" || true ; pwd -P )"
 
 GOOGLE_CLOUD_PROJECT=${1:-$(gcloud config list --format 'value(core.project)')}
 
+ROLE_ID=appengine_deployer_gh_actions
+SERVICE_ACCOUNT=github-actions-deployment
+
+SERVICE_ACCOUNT_EMAIL=${SERVICE_ACCOUNT}@${GOOGLE_CLOUD_PROJECT}.iam.gserviceaccount.com
+
 if gcloud iam roles describe "${ROLE_ID}" --project="${GOOGLE_CLOUD_PROJECT}" > /dev/null 2>&1; then
     echo "✓ Found Role ${ROLE_ID} in project ${GOOGLE_CLOUD_PROJECT}"
 else
@@ -15,7 +20,13 @@ else
     gcloud iam roles create "${ROLE_ID}" --file="$BASEDIR/appengine_deployer_role.yml" --project="${GOOGLE_CLOUD_PROJECT}"
 fi
 
-gcloud iam service-accounts create github-actions-deployment --display-name "github-actions-deployment" --project="${GOOGLE_CLOUD_PROJECT}"
+if gcloud iam service-accounts describe "${SERVICE_ACCOUNT_EMAIL}" --project="${GOOGLE_CLOUD_PROJECT}" > /dev/null 2>&1; then
+  echo "✓ Found Service Account ${SERVICE_ACCOUNT_EMAIL}"
+else
+  echo "… Creating Service Account ${SERVICE_ACCOUNT_EMAIL}"
+  gcloud iam service-accounts create "${SERVICE_ACCOUNT}" --display-name="${SERVICE_ACCOUNT}" --description="Account to deploy via GitHub Actions" --project="${GOOGLE_CLOUD_PROJECT}"
+fi
+
 gcloud projects add-iam-policy-binding "${GOOGLE_CLOUD_PROJECT}" --member="serviceAccount:github-actions-deployment@${GOOGLE_CLOUD_PROJECT}.iam.gserviceaccount.com" --role="projects/${GOOGLE_CLOUD_PROJECT}/roles/appengine_deployer_gh_actions" --condition=None
 
 TMP_OUTPUT_FILE="$(mktemp)"
