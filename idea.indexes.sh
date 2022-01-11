6d884b90500d269a20fcb2daa6f00504b4509b59
Stepan Legachev <gitlog@sib.li>
2022-01-12 02:06:59 +0800

    refactor: move cdn root to the home dir instead of tmp, fixed cdn port instead of random

--- idea.indexes.sh
+++ idea.indexes.sh
@@ -6,9 +6,17 @@ set -euo pipefail
 # USAGE:
 # idea.indexes.sh /path/to/project/dir
 
-INDEX_DIR="/tmp/shared-indexes"
-mkdir -p "$INDEX_DIR"
+CDN_ROOT="$HOME/_idea_cdn"
+mkdir -p "$CDN_ROOT"
 
+TMP_OUTPUT_DIR="$(mktemp -d)"
+function cleanup() {
+  rm -rf "$TMP_OUTPUT_DIR"
+}
+trap cleanup SIGINT
+#trap cleanup ERR
+
+#IDEA_BIN="open -na 'Intellij IDEA' --args"
 IDEA_BIN="/Applications/IntelliJ IDEA.app/Contents/MacOS/idea"
 
 CDN_LAYOUT_TOOL="cdn-layout-tool"
@@ -85,21 +93,42 @@ echo "idea.log.path=/tmp/ide-log" >> "$IDEA_PROPERTIES"
 LAST_COMMIT=$(git --git-dir="$PROJECT_DIR/.git" rev-parse HEAD 2>/dev/null)
 
 # Generate indexes
-"$IDEA_BIN" dump-shared-index project --output="$INDEX_DIR/generate-output" --tmp="$INDEX_DIR/temp" --project-dir="$PROJECT_DIR" --project-id="$PROJECT_NAME" --commit="$LAST_COMMIT"
+echo Creating shared indexes at:
+printbright "$TMP_OUTPUT_DIR"
+printbright "You could later copy produced *.ijx files to the idea.system.path/shared-index"
+"$IDEA_BIN" dump-shared-index project --output="$TMP_OUTPUT_DIR" --tmp="/tmp" --project-dir="$PROJECT_DIR" --project-id="$PROJECT_NAME" --commit="$LAST_COMMIT" --add-hash-to-output-names=false
 
-
-# Random port (RFC 6056), from 1111 to 9999
-PORT=$((1111 + RANDOM % 8888))
+PORT=9876
+URL="http://0.0.0.0:$PORT/"
 
 # Generate CDN directory structure from indexes
-"$CDN_LAYOUT_TOOL" --indexes-dir="$INDEX_DIR/generate-output" --url="http://0.0.0.0:$PORT/"
+echo Creating CDN layout from index chunks...
+"$CDN_LAYOUT_TOOL" --indexes-dir="$TMP_OUTPUT_DIR" --url="$URL"
 
+rm "$TMP_OUTPUT_DIR/project/list.json.xz"
+cp -Rp "$TMP_OUTPUT_DIR/project"/* "$CDN_ROOT"
+rm -rf "$TMP_OUTPUT_DIR/project"
+cp -p "$TMP_OUTPUT_DIR"/*.ijx.xz "$CDN_ROOT"
 
 echo Updating project intellij.yaml
 touch "$PROJECT_DIR/intellij.yaml"
-yq e -i '.sharedIndex.project[0].url = "http://localhost:'"$PORT"'/"' "$PROJECT_DIR/intellij.yaml"
+yq e -i '.sharedIndex.project[0].url = "'$URL$PROJECT_NAME'"' "$PROJECT_DIR/intellij.yaml"
 cat "$PROJECT_DIR/intellij.yaml"
 
-echo Launching local web server with generated CDN layout...
-cd "$INDEX_DIR/generate-output/project/$PROJECT_NAME"
-python3 -m http.server "$PORT"
+echo Launching local web server with generated CDN layout at:
+printbright "$CDN_ROOT"
+
+if hash php 2>/dev/null; then
+    cd "$CDN_ROOT"
+    php -S "0.0.0.0:$PORT"
+elif hash python3 2>/dev/null; then
+    cd "$CDN_ROOT"
+    python3 -m http.server "$PORT"
+elif hash npx 2>/dev/null; then
+    npx http-server "$CDN_ROOT" --port="$PORT"
+else
+    printerr "Could not find python3, php or npx, please launch local web server manually:"
+    printbright "cd $CDN_ROOT"
+    printbright "python3 -m http.server $PORT"
+    exit 3
+fi
