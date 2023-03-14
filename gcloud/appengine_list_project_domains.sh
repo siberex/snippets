49bde80a7e7296d49e45932d0664b3db41d40cac
Stepan Legachev <gitlog@sib.li>
2023-03-14 20:59:06 +0300

    fix: formatting

--- gcloud/appengine_list_project_domains.sh
+++ gcloud/appengine_list_project_domains.sh
@@ -10,6 +10,6 @@
 printf "%s\t%s\t%s\n" "ProjectId" "Region" "Domains"
 gcloud projects list --format 'get(PROJECT_ID)' | while read -r projectId; do
   region=$(gcloud app describe --project "$projectId" --format 'get(locationId)' 2>/dev/null || echo 'N/A')
-  domains=$((gcloud app domain-mappings list --project "$projectId" --format 'get(ID)' 2>/dev/null || echo 'N/A') | xargs | sed 's/ /, /g')
+  domains=$( (gcloud app domain-mappings list --project "$projectId" --format 'get(ID)' 2>/dev/null || echo 'N/A') | xargs | sed 's/ /, /g' )
   printf "%s\t%s\t%s\n" "$projectId" "$region" "$domains"
 done
